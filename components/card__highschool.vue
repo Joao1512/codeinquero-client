623254c34dfcc0ec267d70558a4f5b0363df05ec
Paulo <paulosantos194@hotmail.com>
2019-06-01 20:23:53 -0300

    feat : main card
    Signed-off-by: Paulo <paulosantos194@hotmail.com>

--- components/card__highschool.vue
+++ components/card__highschool.vue
@@ -1,5 +1,16 @@
 <template>
-    
+    <div class="card__wrapper">
+      <img class="logo__facul" src="~/assets/logo_facul.png">
+      <div class="line__divider"></div>
+      <p class="title__facul">Unip</p>
+      <div class="medal">
+        <img class="medal__icon" src="https://img.icons8.com/cotton/50/000000/olympic-medal-silver.png">
+        <p class="text__medal">8623 avaliações de alunos</p>
+      </div>
+      <div class="line__divider"></div>
+      <div class="bolsas__ate"><p>Bolsas de até <font color="#10C979">80%</font></p></div>
+      <div class="a_partir"><p>A partir de <font color="#10C979">R$ 137</font><font size="2" color="#10C979">/mês</font></p></div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +20,61 @@
 </script>
 
 <style scoped>
-
+  .card__wrapper {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    align-items: center;
+    width: 300px;
+    height: 320px;
+    border-radius: 4px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  }
+  .logo__facul {
+    margin-top: 20px;
+    width: 150px;
+    height: 63px;
+  }
+  .line__divider {
+    width: 80%;
+    border: 0.5px solid #C1C1C1;
+    margin: 20px auto;
+  }
+  .title__facul {
+    margin-top: 0;
+    font-family: Roboto;
+    font-style: normal;
+    text-transform: uppercase;
+    color: #4f4f4f;
+  }
+  .medal {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .medal__icon {
+    width: 20px;
+    height: 20px;
+  }
+  .text__medal {
+    font-family: Roboto;
+    font-style: normal;
+    font-size: 12px;
+    color: #888888;
+  }
+  .bolsas__ate {
+    display: flex;
+    flex-direction: row;
+    font-family: Roboto;
+    font-style: normal;
+    color: #888888;
+  }
+  .a_partir {
+    margin-top: -25px;
+    display: flex;
+    flex-direction: row;
+    font-family: Roboto;
+    font-style: normal;
+    color: #888888;
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <bheadercomp></bheadercomp>
+  <div>
+    <bheadercomp></bheadercomp>
+    <p class="content__title">Algumas bolsas de estudo em sua região</p>
+    <div class="content__wrapper">
+      <card__highschool></card__highschool>
+    </div>
+  </div>
 </template>
 
 <script>
   import bheadercomp from '~/components/between_header-and-component.vue'
+  import card__highschool from '~/components/card__highschool.vue'
 
   export default {
     components: {
-      bheadercomp
+      bheadercomp,
+      card__highschool
     }
   }
 </script>
@@ -17,5 +25,20 @@
     margin-left: 0;
     margin-right: 0;
     padding: 0;
+  }
+  .content__wrapper {
+    display: flex;
+    flex-direction: column;
+    max-width: 100vw;
+  }
+  .content__title {
+    font-family: Roboto;
+    font-style: normal;
+    max-width: 250px;
+    text-transform: uppercase;
+    text-align: center;
+    margin: 20px auto;
+    color: #DE9E1F;
+    font-weight: 400;
   }
 </style>

a1452ab165b88573f5c22bd7638ec15ddfba5556
Joao1512 <jvictorsimao@hotmail.com>
2019-06-02 07:43:56 -0300

    status aluno

---
+++ pages/statusaluno.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <statusalunocomponent></statusalunocomponent>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+</style>
+
+<script>
+  import statusalunocomponent from '~/components/statusalunocomponent.vue'
+  export default {
+    components: {
+      statusalunocomponent
+    }
+  }
+</script>

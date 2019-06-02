a4bd066a0b39abbcc0dc0e39be6771d43993bb29
Joao1512 <jvictorsimao@hotmail.com>
2019-06-01 21:17:58 -0300

    so bosta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,67 @@
+
 <template>
   <div class="header">
-    <img class="header__logo" src="~/assets/quero_icon.png">
-    <div class="header__icons-1">
-      <img class="header__icons-logo" src="~/assets/search_icon.png">
-      <label>Busca</label>
+    <div class="header_logo">
+      <img src="~/assets/quero_icon.png">
     </div>
-    <div class="header__linedivider"></div>
-    <div class="header__icons-2">
-      <img class="header__icons-logo" src="~/assets/help_icon.png">
-      <label>Ajuda</label>
-    </div>
-    <div class="header__linedivider"></div>
-    <div class="header__icons-3">
-      <img class="header__icons-logo" src="~/assets/signin_icon.png">
-      <label>Entrar</label>
+    <div class="header_icons">
+      <div class="header_icons-1">
+        <img class="header_icons-logo" src="~/assets/search_icon.png"><br>
+        <label class="label">Busca</label>
       </div>
+      <div class="header_linedivider"></div>
+      <div class="header_icons-2">
+        <img class="header_icons-logo" src="~/assets/help_icon.png"><br>
+        <label>Ajuda</label>
+      </div>
+      <div class="header_linedivider"></div>
+      <div class="header_icons-3">
+        <img class="header_icons-logo" src="~/assets/signin_icon.png"><br>
+        <label>Entrar</label>
+      </div>
+  </div>
   </div>
 </template>
 
 <style>
   .header {
+    font-family: Roboto;
     display: flex;
     flex-direction: row;
     margin: 0 auto;
     justify-content: center;
   }
-  .header__logo {
+  .header_logo {
     width: 94px;
     height: 49px;
     margin: 15px 0 0 -15px;
+    
   }
-  .header__icons {
+  .header_icons {
     display: flex;
+    margin-top:px;
+    margin-left:100px;
+    align-items:flex-end;
     flex-direction: row;
   }
-  .header__icons-logo {
+  .header_icons-1 {
+    text-align: center;
+  }
+  .header_icons-2 {
+    text-align: center;
+  }
+  .header_icons-3 {
+    text-align: center;
+  }
+  .header_icons-logo {
     width: 23px;
     height: 23px;
+  }
+  .header_linedivider {
+    margin-top: 0;
+    margin-left:4px;
+    margin-right:4px;
+    height: 50px;
+    border: 1px solid #C1C1C1
   }
 </style>
